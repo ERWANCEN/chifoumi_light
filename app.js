c13aa3e1ced09feddcbcdb854804927f12d792e3
ERWANCEN <erwance@hotmail.fr>
2025-01-14 11:08:47 +0100

    Exercice complet

---
+++ app.js
@@ -0,0 +1,89 @@
+"use strict";
+
+// chemins vers balises HTML
+const pierre = document.getElementById('pierre');
+const ciseaux = document.getElementById('ciseaux');
+const feuille = document.getElementById('feuille');
+
+const success = document.getElementById('success');
+const monChoix = document.getElementById('mon_choix');
+const choixAdverse = document.getElementById('choix_adverse');
+
+// tableau des 3 choix
+let tab = ['pierre', 'ciseaux', 'feuille'];
+
+
+
+/**
+ * Récupère aléatoirement un chiffre entre 0 et 2
+ * @returns le choix de l'ordinateur
+ */
+function choixDeOrdi() {
+    let indexAleatoire = Math.floor(Math.random() * tab.length);
+    return tab[indexAleatoire];
+}
+
+
+
+/**
+ * Va définir l'issu du match
+ * @param {*} choixJoueur 
+ * @param {*} choixOrdi 
+ * @returns le résultat du match
+ */
+function determinerGagnant(choixJoueur, choixOrdi) {
+    if (choixJoueur === choixOrdi) {
+        return `<p id='egalite'>Match NUL !</p>`;
+    }
+
+    if ((choixJoueur === 'pierre' && choixOrdi === 'ciseaux') || (choixJoueur === 'ciseaux' && choixOrdi === 'feuille') || (choixJoueur === 'feuille' && choixOrdi === 'pierre')) {
+        return `<p id='victoire'>Tu as gagné !</p>`;
+    }
+
+    return `<p id='defaite'>Tu as perdu !</p>`;
+}
+
+
+
+/**
+ * Initiation d'un écouteur d'évènements qui va se déclencher lors d'un clique sur le bouton "pierre" et va afficher le choix du joueur, le choix de l'ordinateur ainsi que le résultat du match
+ */
+pierre.addEventListener('click', () => {
+    let choixJoueur = 'pierre';
+    monChoix.textContent = 'Votre choix : pierre';
+
+    let choixOrdi = choixDeOrdi();
+    choixAdverse.textContent = `Choix de l'ordinateur : ${choixOrdi}`;
+
+    success.innerHTML = `${determinerGagnant(choixJoueur, choixOrdi)}`;
+});
+
+
+
+/**
+ * Initiation d'un écouteur d'évènements qui va se déclencher lors d'un clique sur le bouton "ciseaux" et va afficher le choix du joueur, le choix de l'ordinateur ainsi que le résultat du match
+ */
+ciseaux.addEventListener('click', () => {
+    let choixJoueur = 'ciseaux';
+    monChoix.textContent = 'Votre choix : ciseaux';
+
+    let choixOrdi = choixDeOrdi();
+    choixAdverse.textContent = `Choix de l'ordinateur : ${choixOrdi}`;
+
+    success.innerHTML = `${determinerGagnant(choixJoueur, choixOrdi)}`;
+});
+
+
+
+/**
+ * Initiation d'un écouteur d'évènements qui va se déclencher lors d'un clique sur le bouton "feuille" et va afficher le choix du joueur, le choix de l'ordinateur ainsi que le résultat du match
+ */
+feuille.addEventListener('click', () => {
+    let choixJoueur = 'feuille';
+    monChoix.textContent = 'Votre choix : feuille';
+
+    let choixOrdi = choixDeOrdi();
+    choixAdverse.textContent = `Choix de l'ordinateur : ${choixOrdi}`;
+
+    success.innerHTML = `${determinerGagnant(choixJoueur, choixOrdi)}`;
+});
